90a349c3a5c147d3ee404b4251ec8de9a60d7bdf
Cesar Ancco <canccor@hotmail.com>
2022-06-06 10:17:14 -0500

    Entidad Estadistica

---
+++ app/src/main/java/com/example/laboratorio05/data/estadistica/Estadistica.kt
@@ -0,0 +1,28 @@
+package com.example.laboratorio05.data.estadistica
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName="estadistica")
+class Estadistica(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int=0,
+
+    @ColumnInfo(name="distrito")
+    val distrito: String,
+    @ColumnInfo(name="positivos-vivos")
+    val positivosVivos: Int,
+    @ColumnInfo(name="positivos-defuncion")
+    val positivosDefucion: Int,
+    @ColumnInfo(name="negativo")
+    val negativos: Int,
+    @ColumnInfo(name="pendientes")
+    val pendientes: Int
+
+
+) : Parcelable
